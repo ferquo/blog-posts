f6424757946bb7e5ab10793d548e3420419918dd
Ferenc Pato <ferquo07@gmail.com>
2019-03-28 12:36:15 +0200

    Update blog-post-model.ts

--- src/models/database/blog-post-model.ts
+++ src/models/database/blog-post-model.ts
@@ -1,14 +1,16 @@
 import { DbBaseModel } from './db-base-model';
 import { Entity, Column } from 'typeorm';
-import { Length } from 'class-validator';
+import { Length, IsDefined } from 'class-validator';
 
 @Entity('blogPost')
 export class BlogPostModel extends DbBaseModel {
   @Column()
   @Length(5, 20)
+  @IsDefined()
   title: string;
 
   @Column()
   @Length(10, 200)
+  @IsDefined()
   content: string;
 }
