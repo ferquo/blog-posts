da07fd0cebdb8a00c17794b25a58d79cb3a49ca6
Ferenc Pato <ferquo07@gmail.com>
2019-03-29 11:29:22 +0200

    added tests, and code coverage

--- src/blog/__mocks__/blog-post-service.ts
+++ src/blog/__mocks__/blog-post-service.ts
@@ -2,6 +2,9 @@ import { Operation } from 'fast-json-patch';
 import { NotFoundException } from '@nestjs/common';
 import { CreateBlogPostModel } from '../../models/viewmodel/create-blog-post-model';
 import { GetBlogPostModel } from '../../models/viewmodel/get-blog-post-model';
+import { BlogPostModel } from '../../models/database/blog-post-model';
+import { validate } from 'class-validator';
+import { ValidationErrorException } from '../../shared/exceptions/validation-error.exception';
 
 export class BlogPostService {
     async getBlogPosts(query: any): Promise<GetBlogPostModel[]> {
@@ -11,6 +14,19 @@ export class BlogPostService {
     async createBlogPost(
         blogPost: CreateBlogPostModel,
     ): Promise<GetBlogPostModel> {
+
+        const blogPostValidation = new BlogPostModel();
+        blogPostValidation._id = 'new_id';
+        blogPostValidation.title = blogPost.title;
+        blogPostValidation.content = blogPost.content;
+        blogPostValidation.createdOnDate = blogPostValidation.updatedOnDate = new Date();
+
+        const errors = await validate(blogPostValidation);
+
+        if (errors && errors.length > 0) {
+            throw new ValidationErrorException();
+        }
+
         const newBlogPost = new GetBlogPostModel();
         newBlogPost.id = 'new_id';
         newBlogPost.title = blogPost.title;
--- src/blog/blog-posts/blog-posts.controller.spec.ts
+++ src/blog/blog-posts/blog-posts.controller.spec.ts
@@ -4,6 +4,7 @@ import { BlogPostService } from '../blog-post-service';
 import { GetBlogPostModel } from '../../models/viewmodel/get-blog-post-model';
 import { NotFoundException } from '@nestjs/common';
 import { CreateBlogPostModel } from '../../models/viewmodel/create-blog-post-model';
+import { ValidationErrorException } from '../../shared/exceptions/validation-error.exception';
 // tslint:disable-next-line: no-var-requires
 const mockResponse = require('jest-mock-express').response;
 
@@ -61,5 +62,38 @@ describe('BlogPosts Controller', () => {
 
     expect(res.json).toHaveBeenCalled();
     expect(res.json).toBeCalledWith(expectedResult);
+  });
+
+  it('should not create a blog post, when title is not specified', async () => {
+    const newBlogPost = new CreateBlogPostModel();
+    const expectedResult = new GetBlogPostModel();
+
+    newBlogPost.content = expectedResult.content = 'New Content';
+    expectedResult.id = 'new_id';
+
+    const res = mockResponse();
+
+    expect(
+      controller.createBlogPost(res, newBlogPost),
+    ).rejects.toThrowError(ValidationErrorException);
+
+    expect(res.json).not.toHaveBeenCalled();
+  });
+
+  it('should not create a blog post, when content is not specified', async () => {
+    const newBlogPost = new CreateBlogPostModel();
+    const expectedResult = new GetBlogPostModel();
+
+    newBlogPost.title = expectedResult.title = 'New title';
+
+    expectedResult.id = 'new_id';
+
+    const res = mockResponse();
+
+    expect(controller.createBlogPost(res, newBlogPost)).rejects.toThrowError(
+      ValidationErrorException,
+    );
+
+    expect(res.json).not.toHaveBeenCalled();
   });
 });
